46fc9d58baaae96b661831b7931e274779aed2af
Otapy-pixel <azouan2020@gmail.com>
2026-06-20 05:29:30 +0200

    Update Flask app initialization and port configuration

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask('app')
 
 @app.route('/')
 def home():
     return "Neobank Signature is running!"
 
-port = int(os.environ.get('PORT', 8000))
-app.run(host='0.0.0.0', port=port)
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
