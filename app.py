9f2e4b567202245af909470e0bad6dfd8ac5326d
Otapy-pixel <azouan2020@gmail.com>
2026-06-21 14:11:01 +0200

    Fix indentation for main block in app.py

--- app.py
+++ app.py
@@ -130,6 +130,6 @@ def transfer():
 
 with app.app_context():
     db.create_all()
-    if name == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
